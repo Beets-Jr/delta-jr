39f95c7e10d71427f6c5c855721c78af5ddc8ae8
Matheus Volpon <matheusvvb@hotmail.com>
2021-11-09 11:35:07 -0300

    testes iniciais

--- js/filtro-prof.js
+++ js/filtro-prof.js
@@ -1,4 +1,4 @@
-document.getElementById('filtro').addEventListener('keyup', function() {
+/* document.getElementById('filtro').addEventListener('keyup', function() {
     let pesquisa = this.textContent.toLowerCase()
     let professores = document.querySelectorAll('.professor')
 
@@ -6,4 +6,22 @@ document.getElementById('filtro').addEventListener('keyup', function() {
         let nomeProfessor = i.querySelector('.prof-nome')
         console.log(nomeProfessor)
     }
-})
+}) */
+
+function filterFunction() {
+    var input, filter, ul, li, a, i, value;
+    input = document.querySelector(".searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.querySelector(".animalUL");
+    ul2 = ul.querySelector(".single-animal");
+    li = ul2.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+       a = li[i].getElementsByTagName("span")[0];
+       value = a.textContent || a.innerText;
+       if (value.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+       } else {
+          li[i].style.display = "none";
+       }
+    }
+ }
