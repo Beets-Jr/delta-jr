4377eaa0e01f6b9a728d8c34345d018ff877f65f
Matheus Volpon <matheusvvb@hotmail.com>
2021-11-09 17:58:38 -0300

    other filters structure - partial

--- js/filtro-prof.js
+++ js/filtro-prof.js
@@ -11,3 +11,22 @@ document.getElementById('busca').addEventListener('keyup', function() {
     }
   })
 })
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+	  content.style.flexDirection = "column";
+	  content.style.margin = "0 auto";
+	  content.style.width = "300px";
+	  content.style.gap = "10px";
+    }
+  });
+}
