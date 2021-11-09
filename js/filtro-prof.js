1ae84bb0e0d18987faa77f61284bbe061d08fe85
Matheus Volpon <matheusvvb@hotmail.com>
2021-11-09 16:02:29 -0300

    name filter finished
    Co-authored-by: Gabriel Kyomen <kenkakyomen@hotmail.com>

--- js/filtro-prof.js
+++ js/filtro-prof.js
@@ -1,26 +1,13 @@
-/* document.getElementById('filtro').addEventListener('keyup', function() {
-    let pesquisa = this.textContent.toLowerCase()
-    let professores = document.querySelectorAll('.professor')
+document.getElementById('busca').addEventListener('keyup', function() {
+  let pesquisa = this.value.toLowerCase()
+  let nomesProfessores = document.querySelectorAll('.prof-nome')
 
-    for(let i in professores) {
-        let nomeProfessor = i.querySelector('.prof-nome')
-        console.log(nomeProfessor)
+  nomesProfessores.forEach((nomeProfessor) => {
+    let nomeProfessorTexto =  nomeProfessor.textContent.toLowerCase()
+    if (nomeProfessorTexto.includes(pesquisa)) {
+      nomeProfessor.closest('.professor').classList.remove('hide')
+    } else {
+	    nomeProfessor.closest('.professor').classList.add('hide')
     }
-}) */
-
-function filterFunction() {
-    var input, filter, ul, li, a, i, value;
-    input = document.querySelector(".searchInput");
-    filter = input.value.toUpperCase();
-    ul = document.querySelector(".animalUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-       a = li[i].getElementsByTagName("span")[0];
-       value = a.textContent || a.innerText;
-       if (value.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-       } else {
-          li[i].style.display = "none";
-       }
-    }
- }
+  })
+})
