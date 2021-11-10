6294fa1f03a841b9e57e8e043848cf03b02f8755
Matheus Volpon <matheusvvb@hotmail.com>
2021-11-09 23:17:35 -0300

    filter structure finished

--- js/filtro-prof.js
+++ js/filtro-prof.js
@@ -1,3 +1,4 @@
+// Filtro para nome do professor
 document.getElementById('busca').addEventListener('keyup', function() {
   let pesquisa = this.value.toLowerCase()
   let nomesProfessores = document.querySelectorAll('.prof-nome')
@@ -12,6 +13,7 @@ document.getElementById('busca').addEventListener('keyup', function() {
   })
 })
 
+// Implementação do Collapsible (lista oculta, toggle list)
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -19,14 +21,28 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "flex") {
+    if (content.style.display === "block" || content.style.display === "flex") {
       content.style.display = "none";
     } else {
       content.style.display = "flex";
-	  content.style.flexDirection = "column";
-	  content.style.margin = "0 auto";
-	  content.style.width = "300px";
-	  content.style.gap = "10px";
+      content.style.flexDirection = "column";
+      content.style.margin = "0 auto";
+      content.style.maxWidth = "300px";
+      content.style.gap = "10px";
     }
   });
+}
+
+// Animação do slider - valor máximo da aula (R$)
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+window.onload = function() {
+  document.getElementById('myRange').value = '300';
+  output.innerHTML = document.getElementById('myRange').value;
 }
